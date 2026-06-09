2625a2d30d410f8573c8a79c36f31eba209ab210
datlt024 <letiendat02042001@gmail.com>
2026-06-10 02:11:35 +0900

    fix layout

--- src/features/dictionary/vocabulary/utils/index.ts
+++ src/features/dictionary/vocabulary/utils/index.ts
@@ -1,4 +1,20 @@
+export * from "./formatGlossMeaning"
+export * from "./formatMeaningVi"
+
+export * from "./getHeaderMeaning"
+
+export * from "./getKanjiDisplayMeaning"
+export * from "./getKanjiStrokeCount"
+
+export * from "./getRelationVocabularyList"
+export * from "./getRelationsByType"
+
+export * from "./getSenseGlosses"
+export * from "./getSenseMeaning"
+
 export * from "./getVerbGroupLabel"
-export * from "./getVocabularyMeaning"
-export * from "./getVocabularyPartOfSpeech"
-export * from "./formatMeaning"
+
+export * from "./hasPartOfSpeech"
+
+export * from "./createVocabularyHref"
+export * from "./createKanjiHref"
